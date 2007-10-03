c53e8d7740cb4fc5c17aec5fa6ac5fe47171ee11
zoul <zoul@d38057a2-923b-0410-b96e-e7e898a135ba>
2007-10-03 09:20:27 +0000

    Import dat.
    git-svn-id: http://zoul.fleuron.cz/svn/blatnice@2 d38057a2-923b-0410-b96e-e7e898a135ba

---
+++ trunk/src/fotky.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+while (my $line = <DATA>) {
+	chomp $line;
+	my ($id, $caption) = split(/:/, $line);
+	print<<"cut"
+	<div class="fotka">
+		<a href="fotky/$id.jpg">
+			<img src="nahledy/$id.jpg" alt="$caption">
+		</a>
+		<p>$caption</p>
+	</div>
+cut
+}
+
+__DATA__
+hlavni-budova:Hlavní budova
+veranda:Veranda
+chata:Velká chata
+chatka:Malá chata
+chatky:Malé chatky v horní řadě
+coufalka:„Coufalka“
+horni-hriste-1:Horní hřiště
+horni-hriste-2:Horní hřiště shora
+gol:Horní hřiště v akci
+horni-hriste-3:Horní hřiště v plné šířce
+lod:Loď a rybník volně k mání
+molo-1:Molo směrem z horního hřiště
+molo-2:Molo II
+ohniste:Ohniště
+spodni-hriste:Spodní hřiště
+prijem:Nohejbal na spodním hřišti
+tenis:Tenis na spodním hřišti
+zadni-hriste:Zadní hřiště
+rybnik-1:Rybník I
+rybnik-2:Rybník s plachetnicí
+rybnik-3:Rybník III
+hrib:Takové tu běžně rostou
+snek:Vodní živočich z jiného rybníka
+syslov:V přilehlých lesích se najde leccos
+vazka-1:Vážka I
+vazka-2:Vážka II
+vodomerka:Vodoměrka
